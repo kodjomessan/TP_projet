b750a13eab44e7ade5dce788ae39f5cbf0e442d5
kodjomessan <kodjo.agossou@etu.univ-smb.fr>
2025-03-19 15:57:50 +0100

    fatima

--- ConsoleApplication1/Projet_.cpp
+++ ConsoleApplication1/Projet_.cpp
@@ -7,6 +7,7 @@ int main()
 {
     int nb = 13;
     std::cout << "Hello World!\n";
+    std::cout << "fatima qui casse tout\n";
 }
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
